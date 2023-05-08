4ccda6ab8dfed9ed788a4d4f4b9752bf0096456b
yamiko <kanako.sakiyama1027@gmail.com>
2023-05-09 07:48:31 +0900

    一部の記述を変更

--- src/client/Header.tsx
+++ src/client/Header.tsx
@@ -148,106 +148,85 @@ export const Header: FC = () => {
                 color: theme.palette.customDarkGreen.main,
               }}
             />
-            <div>
-              <IconButton
-                onClick={handleClick}
-                size="small"
-                sx={{ ml: 2 }}
-                aria-controls={open ? "account-menu" : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? "true" : undefined}
-              >
-                <Avatar
-                  src={loginUser?.profilePhoto}
-                  alt={loginUser?.name}
-                  sx={{ width: 40, height: 40 }}
-                />
-              </IconButton>
-              {/* <Button
-                id="basic-button"
-                aria-controls={open ? "basic-menu" : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? "true" : undefined}
-                onClick={handleClick}
-              >
-                <Avatar
-                  src={loginUser?.profilePhoto}
-                  alt={loginUser?.name}
-                  sx={{ width: 40, height: 40 }}
-                />
-              </Button> */}
-              <Menu
-                anchorEl={anchorEl}
-                id="account-menu"
-                open={open}
-                onClose={handleClose}
-                onClick={handleClose}
-                PaperProps={{
-                  elevation: 0,
-                  sx: {
-                    overflow: "visible",
-                    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-                    mt: 1.5,
-                    "& .MuiAvatar-root": {
-                      width: 32,
-                      height: 32,
-                      ml: -0.5,
-                      mr: 1,
-                    },
-                    "&:before": {
-                      content: '""',
-                      display: "block",
-                      position: "absolute",
-                      top: 0,
-                      right: 14,
-                      width: 10,
-                      height: 10,
-                      bgcolor: "background.paper",
-                      transform: "translateY(-50%) rotate(45deg)",
-                      zIndex: 0,
-                    },
-                  },
-                }}
-                transformOrigin={{ horizontal: "right", vertical: "top" }}
-                anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-              >
-                {/* <Menu
-                id="basic-menu"
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                MenuListProps={{
-                  "aria-labelledby": "basic-button",
-                }}
-              > */}
-                <MenuItem onClick={profileHandler}>
-                  {/* <PersonIcon fontSize="small" /> */}
-                  <ListItemIcon>
-                    <BsFillPersonFill />
-                  </ListItemIcon>
-                  マイページへ
-                </MenuItem>
-                <Divider />
-                <MenuItem onClick={logoutHandler}>
-                  {/* <Logout fontSize="small" /> */}
-                  <ListItemIcon>
-                    <FiLogOut />
-                  </ListItemIcon>
-                  ログアウトする
-                </MenuItem>
-                <Divider />
-                <MenuItem
-                  onClick={withdrawalHandler}
-                  sx={{ color: theme.palette.error.main }}
+            {loginUser ? (
+              <div>
+                <IconButton
+                  onClick={handleClick}
+                  size="small"
+                  sx={{ ml: 2 }}
+                  aria-controls={open ? "account-menu" : undefined}
+                  aria-haspopup="true"
+                  aria-expanded={open ? "true" : undefined}
                 >
-                  {/* <PersonOffIcon fontSize="small" /> */}
-                  <ListItemIcon>
-                    <MdPersonOff />
-                  </ListItemIcon>
-                  退会する
-                </MenuItem>
-              </Menu>
-            </div>
+                  <Avatar
+                    src={loginUser?.profilePhoto}
+                    alt={loginUser?.name}
+                    sx={{ width: 40, height: 40 }}
+                  />
+                </IconButton>
+                <Menu
+                  anchorEl={anchorEl}
+                  id="account-menu"
+                  open={open}
+                  onClose={handleClose}
+                  onClick={handleClose}
+                  PaperProps={{
+                    elevation: 0,
+                    sx: {
+                      overflow: "visible",
+                      filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                      mt: 1.5,
+                      "& .MuiAvatar-root": {
+                        width: 32,
+                        height: 32,
+                        ml: -0.5,
+                        mr: 1,
+                      },
+                      "&:before": {
+                        content: '""',
+                        display: "block",
+                        position: "absolute",
+                        top: 0,
+                        right: 14,
+                        width: 10,
+                        height: 10,
+                        bgcolor: "background.paper",
+                        transform: "translateY(-50%) rotate(45deg)",
+                        zIndex: 0,
+                      },
+                    },
+                  }}
+                  transformOrigin={{ horizontal: "right", vertical: "top" }}
+                  anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+                >
+                  <MenuItem onClick={profileHandler}>
+                    <ListItemIcon>
+                      <BsFillPersonFill />
+                    </ListItemIcon>
+                    マイページへ
+                  </MenuItem>
+                  <Divider />
+                  <MenuItem onClick={logoutHandler}>
+                    <ListItemIcon>
+                      <FiLogOut />
+                    </ListItemIcon>
+                    ログアウトする
+                  </MenuItem>
+                  <Divider />
+                  <MenuItem
+                    onClick={withdrawalHandler}
+                    sx={{ color: theme.palette.error.main }}
+                  >
+                    <ListItemIcon>
+                      <MdPersonOff />
+                    </ListItemIcon>
+                    退会する
+                  </MenuItem>
+                </Menu>
+              </div>
+            ) : (
+              <div></div>
+            )}
           </Grid>
         </Container>
       </Sheader>
