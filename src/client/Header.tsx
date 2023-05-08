fc3603b9c8fad10e78dd640ff614ea4ce83b43a0
yamiko <kanako.sakiyama1027@gmail.com>
2023-05-09 05:54:12 +0900

    一部の記述を変更

--- src/client/Header.tsx
+++ src/client/Header.tsx
@@ -2,7 +2,14 @@ import { FC, MouseEvent, useState } from "react";
 import { useRouter } from "next/router";
 
 import Image from "next/image";
-import { Container, Divider, ListItemIcon, styled } from "@mui/material";
+import {
+  Avatar,
+  Container,
+  Divider,
+  IconButton,
+  ListItemIcon,
+  styled,
+} from "@mui/material";
 import Button from "@mui/material/Button";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
@@ -142,16 +149,69 @@ export const Header: FC = () => {
               }}
             />
             <div>
-              <Button
+              <IconButton
+                onClick={handleClick}
+                size="small"
+                sx={{ ml: 2 }}
+                aria-controls={open ? "account-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+              >
+                <Avatar
+                  src={loginUser?.profilePhoto}
+                  alt={loginUser?.name}
+                  sx={{ width: 40, height: 40 }}
+                />
+              </IconButton>
+              {/* <Button
                 id="basic-button"
                 aria-controls={open ? "basic-menu" : undefined}
                 aria-haspopup="true"
                 aria-expanded={open ? "true" : undefined}
                 onClick={handleClick}
               >
-                MENU
-              </Button>
+                <Avatar
+                  src={loginUser?.profilePhoto}
+                  alt={loginUser?.name}
+                  sx={{ width: 40, height: 40 }}
+                />
+              </Button> */}
               <Menu
+                anchorEl={anchorEl}
+                id="account-menu"
+                open={open}
+                onClose={handleClose}
+                onClick={handleClose}
+                PaperProps={{
+                  elevation: 0,
+                  sx: {
+                    overflow: "visible",
+                    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                    mt: 1.5,
+                    "& .MuiAvatar-root": {
+                      width: 32,
+                      height: 32,
+                      ml: -0.5,
+                      mr: 1,
+                    },
+                    "&:before": {
+                      content: '""',
+                      display: "block",
+                      position: "absolute",
+                      top: 0,
+                      right: 14,
+                      width: 10,
+                      height: 10,
+                      bgcolor: "background.paper",
+                      transform: "translateY(-50%) rotate(45deg)",
+                      zIndex: 0,
+                    },
+                  },
+                }}
+                transformOrigin={{ horizontal: "right", vertical: "top" }}
+                anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+              >
+                {/* <Menu
                 id="basic-menu"
                 anchorEl={anchorEl}
                 open={open}
@@ -159,7 +219,7 @@ export const Header: FC = () => {
                 MenuListProps={{
                   "aria-labelledby": "basic-button",
                 }}
-              >
+              > */}
                 <MenuItem onClick={profileHandler}>
                   {/* <PersonIcon fontSize="small" /> */}
                   <ListItemIcon>
@@ -167,7 +227,7 @@ export const Header: FC = () => {
                   </ListItemIcon>
                   マイページへ
                 </MenuItem>
-                <Divider/>
+                <Divider />
                 <MenuItem onClick={logoutHandler}>
                   {/* <Logout fontSize="small" /> */}
                   <ListItemIcon>
@@ -175,7 +235,7 @@ export const Header: FC = () => {
                   </ListItemIcon>
                   ログアウトする
                 </MenuItem>
-                <Divider/>
+                <Divider />
                 <MenuItem
                   onClick={withdrawalHandler}
                   sx={{ color: theme.palette.error.main }}
